65254d8ec3d48d34b6d561f0121b58f6a8621777
Andreas Schneider <asn@cryptomilk.org>
2021-06-07 13:23:02 -0400

    hfp: Report SCO sample rate via g_sco_samplerate audio param if needed
    On Samsung devices, we need to tell Audio HAL if we're running narrow
    band or wide band.
    
    Change-Id: Ibfa0f632d9acbb920a85a613ce4e2f1d26556bd1
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- src/com/android/bluetooth/btservice/AdapterService.java
+++ src/com/android/bluetooth/btservice/AdapterService.java
@@ -4002,4 +4002,8 @@ public class AdapterService extends Service {
     public boolean isMock() {
         return false;
     }
+
+    public boolean shouldReportScoSampleRate() {
+        return getResources().getBoolean(com.android.bluetooth.R.bool.sco_report_samplerate);
+    }
 }
